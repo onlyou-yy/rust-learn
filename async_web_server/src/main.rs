15aeab7bf1e44521c56611138cb34368818d7e11
练建发 <958823465@qq.com>
2024-10-31 11:04:39 +0800

    测试 handle_connection 函数

--- async_web_server/src/main.rs
+++ async_web_server/src/main.rs
@@ -1,6 +1,5 @@
 use std::{fs, time::Duration};
 use async_std::net::TcpListener;
-use async_std::net::TcpStream;
 use async_std::task;
 use async_std::prelude::*;
 use futures::StreamExt;
@@ -23,7 +22,7 @@ async fn main() {
 
 }
 
-async fn handle_connection(mut stream:TcpStream) {
+async fn handle_connection(mut stream:impl async_std::io::Read + async_std::io::Write + std::marker::Unpin) {
     let mut buffer = [0;1024];
     stream.read(&mut buffer).await.unwrap();
 
@@ -49,4 +48,64 @@ async fn handle_connection(mut stream:TcpStream) {
     let response = format!("{status_line}{content}");
     stream.write_all(response.as_bytes()).await.unwrap();
     stream.flush().await.unwrap();
+}
+
+struct MockTcpStream {
+    read_data:Vec<u8>,
+    write_data:Vec<u8>,
+}
+
+impl async_std::io::Read for MockTcpStream {
+    fn poll_read(
+        self: std::pin::Pin<&mut Self>,
+        _: &mut std::task::Context<'_>,
+        buf: &mut [u8],
+    ) -> std::task::Poll<std::io::Result<usize>> {
+        let size = std::cmp::min(self.read_data.len(), buf.len());
+        buf[..size].copy_from_slice(&self.read_data[..size]);
+        futures::task::Poll::Ready(Ok(size))
+    }
+}
+
+impl async_std::io::Write for MockTcpStream {
+    fn poll_write(
+        mut self: std::pin::Pin<&mut Self>,
+        _: &mut std::task::Context<'_>,
+        buf: &[u8],
+    ) -> std::task::Poll<std::io::Result<usize>> {
+        self.write_data = Vec::from(buf);
+        futures::task::Poll::Ready(Ok(buf.len()))
+    }
+
+    fn poll_flush(self: std::pin::Pin<&mut Self>, _: &mut std::task::Context<'_>) -> std::task::Poll<std::io::Result<()>> {
+       futures::task::Poll::Ready(Ok(())) 
+    }
+
+    fn poll_close(self: std::pin::Pin<&mut Self>, _: &mut std::task::Context<'_>) -> std::task::Poll<std::io::Result<()>> {
+       futures::task::Poll::Ready(Ok(())) 
+    }
+}
+
+impl std::marker::Unpin for MockTcpStream {}
+
+
+#[async_std::test]
+async fn test_handle_connection() {
+    let input_bytes = b"GET / HTTP/1.1\r\n";
+    let mut content = vec![0u8;1024];
+    content[..input_bytes.len()].clone_from_slice(input_bytes);
+
+    let mut stream = MockTcpStream {
+        read_data:content,
+        write_data:Vec::new(),
+    };
+
+    handle_connection(&mut stream).await;
+    let mut buf = [0u8;1024];
+    stream.read(&mut buf).await.unwrap();
+
+    let expected_contents = fs::read_to_string("hello.html").unwrap();
+    let expected_response = format!("HTTP/1.1 200 OK\r\n\r\n{expected_contents}");
+
+    assert!(stream.write_data.starts_with(expected_response.as_bytes()));
 }
