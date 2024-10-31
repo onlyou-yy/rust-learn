d5711cdc76ee1b51269232c16db8ca31b9251cb1
练建发 <958823465@qq.com>
2024-10-31 20:26:26 +0800

    多进程共享状态

--- my_redis/src/main.rs
+++ my_redis/src/main.rs
@@ -1,5 +1,8 @@
+use std::{collections::HashMap, sync::{Arc, Mutex}};
+use bytes::Bytes;
 use tokio::net::{TcpListener, TcpStream};
 use mini_redis::{Connection, Frame};
+
 
 #[tokio::main]
 async fn main() {
@@ -7,19 +10,32 @@ async fn main() {
     // 监听指定地址，等待 TCP 连接进来
     let listener = TcpListener::bind("127.0.0.1:6379").await.unwrap();
 
+    let db = Arc::new(Mutex::new(HashMap::new()));
+
     loop {
         // 第二个被忽略的项中包含有新连接的 `IP` 和端口信息
         let (socket, _) = listener.accept().await.unwrap();
-        process(socket).await;
+        let db = db.clone();
+
+        tokio::spawn(async move {
+            process(socket,db).await;
+        });
+
     }
 }
 
-async fn process(socket: TcpStream) {
-    use mini_redis::Command::{self, Get, Set};
-    use std::collections::HashMap;
+type  Db = Arc<Mutex<HashMap<String,Bytes>>>;
+type SharedDb = Arc<Vec<Mutex<HashMap<String,Bytes>>>>;
 
-    // 使用 hashmap 来存储 redis 的数据
-    let mut db = HashMap::new();
+fn new_shared_db(num_shareds:usize) -> SharedDb {
+    let mut db = Vec::with_capacity(num_shareds);
+    for _ in 0..num_shareds {
+        db.push(Mutex::new(HashMap::new()));
+    }
+    Arc::new(db)
+}
+async fn process(socket: TcpStream,db:Db) {
+    use mini_redis::Command::{self, Get, Set};
 
     // `mini-redis` 提供的便利函数，使用返回的 `connection` 可以用于从 socket 中读取数据并解析为数据帧
     let mut connection = Connection::new(socket);
@@ -29,10 +45,12 @@ async fn process(socket: TcpStream) {
         let response = match Command::from_frame(frame).unwrap() {
             Set(cmd) => {
                 // 值被存储为 `Vec<u8>` 的形式
-                db.insert(cmd.key().to_string(), cmd.value().to_vec());
+                let mut db = db.lock().unwrap();
+                db.insert(cmd.key().to_string(), Bytes::from(cmd.value().to_vec()));
                 Frame::Simple("OK".to_string())
             }
             Get(cmd) => {
+                let db = db.lock().unwrap();
                 if let Some(value) = db.get(cmd.key()) {
                     // `Frame::Bulk` 期待数据的类型是 `Bytes`， 该类型会在后面章节讲解，
                     // 此时，你只要知道 `&Vec<u8>` 可以使用 `into()` 方法转换成 `Bytes` 类型
