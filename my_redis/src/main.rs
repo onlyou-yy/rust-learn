69ed527049f6a9bae96f5ac32f2a22fdd91fb525
练建发 <958823465@qq.com>
2024-10-31 16:13:45 +0800

    接收并返回数据

--- my_redis/src/main.rs
+++ my_redis/src/main.rs
@@ -15,15 +15,36 @@ async fn main() {
 }
 
 async fn process(socket: TcpStream) {
-    // `Connection` 对于 redis 的读写进行了抽象封装，因此我们读到的是一个一个数据帧frame(数据帧 = redis命令 + 数据)，而不是字节流
-    // `Connection` 是在 mini-redis 中定义
+    use mini_redis::Command::{self, Get, Set};
+    use std::collections::HashMap;
+
+    // 使用 hashmap 来存储 redis 的数据
+    let mut db = HashMap::new();
+
+    // `mini-redis` 提供的便利函数，使用返回的 `connection` 可以用于从 socket 中读取数据并解析为数据帧
     let mut connection = Connection::new(socket);
 
-    if let Some(frame) = connection.read_frame().await.unwrap() {
-        println!("GOT: {:?}", frame);
+    // 使用 `read_frame` 方法从连接获取一个数据帧：一条redis命令 + 相应的数据
+    while let Some(frame) = connection.read_frame().await.unwrap() {
+        let response = match Command::from_frame(frame).unwrap() {
+            Set(cmd) => {
+                // 值被存储为 `Vec<u8>` 的形式
+                db.insert(cmd.key().to_string(), cmd.value().to_vec());
+                Frame::Simple("OK".to_string())
+            }
+            Get(cmd) => {
+                if let Some(value) = db.get(cmd.key()) {
+                    // `Frame::Bulk` 期待数据的类型是 `Bytes`， 该类型会在后面章节讲解，
+                    // 此时，你只要知道 `&Vec<u8>` 可以使用 `into()` 方法转换成 `Bytes` 类型
+                    Frame::Bulk(value.clone().into())
+                } else {
+                    Frame::Null
+                }
+            }
+            cmd => panic!("unimplemented {:?}", cmd),
+        };
 
-        // 回复一个错误
-        let response = Frame::Error("unimplemented".to_string());
+        // 将请求响应返回给客户端
         connection.write_frame(&response).await.unwrap();
     }
 }
